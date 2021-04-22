96a95dc5f9146a6ef99cfe2f90f9d646c722d117
Nick Feng <fdj32@163.com>
2021-04-22 12:54:25 +0800

    7

--- static/js/main.js
+++ static/js/main.js
@@ -13,7 +13,7 @@ var blog_contents = [
         contents: [
             {
                 title: 'j.u.c.l.AbstractQueuedSynchronizer',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/java/util/concurrent/locks/AbstractQueuedSynchronizer.java'
+                link: 'java/util/concurrent/locks/AbstractQueuedSynchronizer.java'
             }
         ]
     },
@@ -22,7 +22,7 @@ var blog_contents = [
         contents: [
             {
                 title: 'Line separator in awk/sed',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-07.line.separator.awk.sed.md'
+                link: 'blog/2021-04-07.line.separator.awk.sed.md'
             }
         ]
     },
@@ -31,43 +31,43 @@ var blog_contents = [
         contents: [
             {
                 title: '垃圾回收优化简介',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-09.gc-01-introduction-garbage-collection-tuning.md'
+                link: 'blog/2021-04-09.gc-01-introduction-garbage-collection-tuning.md'
             },
             {
                 title: '人机工程',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-11.gc-02-ergonomics.md'
+                link: 'blog/2021-04-11.gc-02-ergonomics.md'
             },
             {
                 title: '垃圾收集器实现',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-12.gc-03-garbage-collector-implementation.md'
+                link: 'blog/2021-04-12.gc-03-garbage-collector-implementation.md'
             },
             {
                 title: '影响垃圾收集性能的因素',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-13.gc-04-factors-affecting-garbage-collection-performance.md'
+                link: 'blog/2021-04-13.gc-04-factors-affecting-garbage-collection-performance.md'
             },
             {
                 title: '可用的收集器',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-13.gc-05-available-collectors.md'
+                link: 'blog/2021-04-13.gc-05-available-collectors.md'
             },
             {
                 title: '并行收集器',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-13.gc-06-parallel-collector.md'
+                link: 'blog/2021-04-13.gc-06-parallel-collector.md'
             },
             {
                 title: '垃圾优先垃圾收集器',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-16.gc-07-garbage-first-g1-garbage-collector.md'
+                link: 'blog/2021-04-16.gc-07-garbage-first-g1-garbage-collector.md'
             },
             {
                 title: '垃圾优先垃圾收集器调优',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-19.gc-08-garbage-first-garbage-collector-tuning.md'
+                link: 'blog/2021-04-19.gc-08-garbage-first-garbage-collector-tuning.md'
             },
             {
                 title: 'Z 垃圾收集器',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-20.gc-09-z-garbage-collector.md'
+                link: 'blog/2021-04-20.gc-09-z-garbage-collector.md'
             },
             {
                 title: '其他考虑',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-20.gc-10-other-considerations.md'
+                link: 'blog/2021-04-20.gc-10-other-considerations.md'
             }
         ]
     },
@@ -76,11 +76,11 @@ var blog_contents = [
         contents: [
             {
                 title: 'JVM技术概述',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-08.java-virtual-machine-technology-overview.md'
+                link: 'blog/2021-04-08.java-virtual-machine-technology-overview.md'
             },
             {
                 title: '编译器控制',
-                link: 'https://github.com/fdj32/fdj32.github.io/blob/master/blog/2021-04-08.jvm-guide-02-compiler-control.md'
+                link: 'blog/2021-04-08.jvm-guide-02-compiler-control.md'
             }
         ]
     },
